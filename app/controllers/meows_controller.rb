87bbedfe8c3fa3c34ce73adae99358f1a66fe9a5
Kelly Everett <dkever79@yahoo.com>
2016-03-04 15:03:53 -0600

    growl

--- app/controllers/meows_controller.rb
+++ app/controllers/meows_controller.rb
@@ -1,10 +1,20 @@
 class MeowsController < ApplicationController
   set :views, Proc.new { File.join(root, "views/meows")}
 
+before do
+  session[:meows_array] ||= []
+end
+
   get '/meows' do
     @meows = Meow.all
     erb :index
   end
 
+post '/meows' do
+    meow = Meow.new(params["meow"])
+    meow.save
+    session[:meow]
+    redirect '/meows'
+  end
 
 end
